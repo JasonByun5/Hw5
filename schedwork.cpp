fb6b1fc679725d7da17452640443dcc589bb9b5e
Jason Byun <jsbyun@usc.edu>
2025-04-25 05:51:19 +0000

    everything finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shiftsWorked,
+    size_t day,
+    size_t slot
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +46,56 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t n = avail.size();
+    size_t k = avail[0].size();
 
+    sched.clear();
+    for(size_t i =0; i<n; ++i){
+        vector<Worker_T> daySchedule;
+        for(size_t j =0; j < dailyNeed; ++j){
+            daySchedule.push_back(INVALID_ID);
+        }
+        sched.push_back(daySchedule);
+    }
 
+    vector<size_t> shiftsWorked(k, 0);
+
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorked, 0, 0);
 
 }
 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shiftsWorked,
+    size_t day,
+    size_t slot)
+{
+    if(day == sched.size()){
+        return true;
+    }
+
+    if(slot == dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorked, day + 1, 0);
+    }
+
+    for(size_t worker = 0; worker < avail[0].size(); ++worker){
+        if(avail[day][worker] && shiftsWorked[worker] < maxShifts &&
+            find(sched[day].begin(), sched[day].end(), worker) == sched[day].end())
+        {
+        
+            sched[day][slot] = worker;
+            shiftsWorked[worker]++;
+
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorked, day, slot + 1)){
+                return true; 
+            }
+
+            sched[day][slot] = INVALID_ID;
+            shiftsWorked[worker]--;
+        }
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,12 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+    const std::string& current,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    set<string>& results,
+    int index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +26,54 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+  set<string> results;
+  wordleHelper(in, floating, dict, results, 0);
+  return results;
 }
 
 // Define any helper functions here
+void wordleHelper(
+    const std::string& current,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    set<string>& results,
+    int index)
+{
+  if(index == current.size()){
+    if(floating.empty() && dict.find(current) != dict.end()){
+      results.insert(current);
+    }
+
+    return;
+  }
+
+  if (current[index] != '-'){
+    wordleHelper(current, floating, dict, results, index + 1);
+  }else{
+    
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(size_t i = 0; i <alphabet.size(); ++i){
+      char ch = alphabet[i];
+      
+      string newWord = current;
+      newWord[index] = ch;
+
+      bool found = false;
+      size_t pos = 0;
+
+      while(pos < floating.size()){
+        if(floating[pos] == ch){
+          found = true;
+          break;
+        }
+        ++pos;
+      }
+      std::string newFloating = floating;
+      if(found){
+        newFloating.erase(pos, 1);
+      }
+
+      wordleHelper(newWord, newFloating, dict, results, index +1);
+    }
+  }
+}
