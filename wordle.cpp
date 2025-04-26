40a31c865baddd419f67e618998210d6e4ab9c59
Jason Byun <jsbyun@usc.edu>
2025-04-26 00:55:54 +0000

    different method tried for wordle

--- wordle.cpp
+++ wordle.cpp
@@ -1,24 +1,37 @@
 #ifndef RECCHECK
 // For debugging
 #include <iostream>
+#include <string>
 // For std::remove
 #include <algorithm> 
-#include <map>
 #include <set>
 #endif
 
 #include "wordle.h"
 #include "dict-eng.h"
+
 using namespace std;
 
 
 // Add prototypes of helper functions here
-void wordleHelper(
-    const std::string& current,
-    const std::string& floating,
-    const std::set<std::string>& dict,
-    set<string>& results,
-    int index);
+static int countEmpty(const string& in){
+  int cnt = 0;
+  for(char c: in){
+    if(c == '-'){
+      cnt++;
+    }
+  }
+
+  return cnt;
+}
+
+void helper(
+  std::string& currentWord,
+  const std::string& floating,
+  const std::set<std::string>& dict,
+  std::set<std::string>& results,
+  int index,
+  int empty);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -26,54 +39,53 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-  set<string> results;
-  wordleHelper(in, floating, dict, results, 0);
+  std::set<std::string> results;
+  std::string currentWord = in;
+  int empty = countEmpty(in);
+  helper(currentWord, floating, dict, results, 0, empty);
+
   return results;
 }
 
-// Define any helper functions here
-void wordleHelper(
-    const std::string& current,
-    const std::string& floating,
-    const std::set<std::string>& dict,
-    set<string>& results,
-    int index)
-{
-  if(index == current.size()){
-    if(floating.empty() && dict.find(current) != dict.end()){
-      results.insert(current);
-    }
+void helper(
+  std::string& currentWord,
+  const std::string& floating,
+  const std::set<std::string>& dict,
+  std::set<std::string>& results,
+  int index,
+  int empty){
 
+    int n = currentWord.size();
+
+  if(index == n){
+    if(floating.empty() && dict.count(currentWord)){
+      results.insert(currentWord);
+    }
+    
     return;
   }
 
-  if (current[index] != '-'){
-    wordleHelper(current, floating, dict, results, index + 1);
-  }else{
-    
-    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    for(size_t i = 0; i <alphabet.size(); ++i){
-      char ch = alphabet[i];
-      
-      string newWord = current;
-      newWord[index] = ch;
 
-      bool found = false;
-      size_t pos = 0;
 
-      while(pos < floating.size()){
-        if(floating[pos] == ch){
-          found = true;
-          break;
-        }
-        ++pos;
-      }
-      std::string newFloating = floating;
-      if(found){
-        newFloating.erase(pos, 1);
-      }
-
-      wordleHelper(newWord, newFloating, dict, results, index +1);
-    }
+  if (currentWord[index] != '-'){
+    helper(currentWord, floating, dict, results, index + 1, empty);
+    return;
   }
+
+  for(char c = 'a'; c<= 'z'; ++c){
+    currentWord[index] = c;
+    
+    size_t pos = floating.find(c);
+    if(pos != string::npos){
+      string newFloating = floating.substr(0, pos) + floating.substr(pos + 1);
+      helper(currentWord, newFloating, dict, results, index + 1, empty - 1);
+    }
+    else if(empty > (int)floating.size()){
+      helper(currentWord, floating, dict, results, index + 1, empty - 1);
+    }
+
+    currentWord[index] = '-';
+  }
+
+
 }
